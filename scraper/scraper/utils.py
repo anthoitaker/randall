ef8e40c1a801f34734105c91476166112b2d3169
Toni Robles <r21robles@gmail.com>
2018-03-23 23:50:39 +0100

    Use scrapydo in import_trouble function

--- scraper/scraper/utils.py
+++ scraper/scraper/utils.py
@@ -1,12 +1,13 @@
 import os
-from scrapy.crawler import CrawlerProcess
+import scrapydo
 from scrapy.utils.project import get_project_settings
 from scraper.scraper.spiders.dtc_spider import DtcSpider
 from config.settings import SCRAPY_SETTINGS_MODULE
 
 
+scrapydo.setup()
+
 def import_trouble(code):
     os.environ.setdefault('SCRAPY_SETTINGS_MODULE', SCRAPY_SETTINGS_MODULE)
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(DtcSpider, code=code)
-    process.start()
+    scraper_settings = get_project_settings()
+    scrapydo.run_spider(DtcSpider, settings=scraper_settings, code=code)
