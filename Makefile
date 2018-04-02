f8dbc6921d4d4ee70ed9bc0e1eae251405c41999
Toni Robles <r21robles@gmail.com>
2018-04-02 11:55:58 +0200

    Add fabfile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 DOCKER_COMPOSE = docker-compose -f docker-compose.dev.yml
 DRUN = $(DOCKER_COMPOSE) run --rm
 
-.PHONY: run inspect build push
+.PHONY: run inspect build push update deploy
 
 run:
 		$(DRUN) --name randall-service --service-ports randall
@@ -14,3 +14,9 @@ build:
 
 push:
 		docker push anthoitaker/randall
+
+update:
+		$(DRUN) --name randall-fab fabric update
+
+deploy:
+		$(DRUN) --name randall-fab fabric deploy
---
+++ fabfile.py
@@ -0,0 +1,17 @@
+import os
+from fabric.api import cd, env, put, run
+
+REMOTE_HOST = os.getenv('REMOTE_HOST')
+REMOTE_USER = os.getenv('REMOTE_USER')
+REMOTE_APP_DIR = os.getenv('REMOTE_APP_DIR')
+
+env.hosts = ['{user}@{host}'.format(user=REMOTE_USER, host=REMOTE_HOST)]
+
+def update():
+    with cd(REMOTE_APP_DIR):
+        put('docker-compose.yml', 'deploy.yml')
+
+def deploy():
+    with cd(REMOTE_APP_DIR):
+        run('docker-compose -f deploy.yml pull')
+        run('docker-compose -f deploy.yml restart')
