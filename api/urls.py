267e9577d293565cc4ab6cf3a5b7e81233637653
Toni Robles <r21robles@gmail.com>
2018-06-23 17:13:52 +0200

    Allow trouble code with capital letter in api endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from . import views
 
-DTC_PARAM = '(?P<dtc>[a-z0-9]{5})'
+DTC_PARAM = '(?P<dtc>[a-zA-Z0-9]{5})'
 
 urlpatterns = [
     path('troubles/', views.TroubleList.as_view()),
