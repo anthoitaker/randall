2c9f82dc0ee1a64832995bdceac0f63a575e10b0
Toni Robles <r21robles@gmail.com>
2018-07-14 10:50:48 +0200

    Add pagination, ordering and filters in list troubles API endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -7,9 +7,6 @@ class TroubleSerializer(serializers.Serializer):
     original_title = serializers.CharField(max_length=200)
     description = serializers.CharField()
     system = serializers.CharField(max_length=100)
-    symptoms = serializers.ListField(child=serializers.CharField())
-    causes = serializers.ListField(child=serializers.CharField())
-    solutions = serializers.ListField(child=serializers.CharField())
 
     def create(self, validated_data):
         raise NotImplementedError()
@@ -24,7 +21,17 @@ class TroubleSerializer(serializers.Serializer):
             'original_title': instance.original_title,
             'description': instance.description,
             'system': instance.get_system_name(),
-            'symptoms': instance.list_symptoms(),
-            'causes': instance.list_causes(),
-            'solutions': instance.list_solutions(),
         }
+
+
+class ExtendedTroubleSerializer(TroubleSerializer):
+    symptoms = serializers.ListField(child=serializers.CharField())
+    causes = serializers.ListField(child=serializers.CharField())
+    solutions = serializers.ListField(child=serializers.CharField())
+
+    def to_representation(self, instance):
+        representation = super(ExtendedTroubleSerializer, self).to_representation(instance)
+        representation['symptoms'] = instance.list_symptoms()
+        representation['causes'] = instance.list_causes()
+        representation['solutions'] = instance.list_solutions()
+        return representation
--- api/views.py
+++ api/views.py
@@ -3,12 +3,37 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from core.models import Trouble
-from .serializers import TroubleSerializer
+from .pagination import PageSizePagination
+from .serializers import TroubleSerializer, ExtendedTroubleSerializer
 
 
 class TroubleList(ListCreateAPIView):
-    queryset = Trouble.objects.all()
-    serializer_class = TroubleSerializer
+    ORDERING_TYPES = ['code', '-code', 'title', 'system']
+    DEFAULT_ORDER = 'code'
+
+    MODE_TYPES = ['simple', 'extended']
+    DEFAULT_MODE = 'simple'
+
+    pagination_class = PageSizePagination
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+
+        mode_param = request.query_params.get('mode', self.DEFAULT_MODE)
+        mode = mode_param if mode_param in self.MODE_TYPES else self.DEFAULT_MODE
+
+        if mode == 'extended':
+            serializer = ExtendedTroubleSerializer(page, many=True)
+        else:
+            serializer = TroubleSerializer(page, many=True)
+
+        return self.get_paginated_response(serializer.data)
+
+    def get_queryset(self):
+        order_param = self.request.query_params.get('order', self.DEFAULT_ORDER)
+        order = order_param if order_param in self.ORDERING_TYPES else self.DEFAULT_ORDER
+        return Trouble.objects.order_by(order)
 
 
 class TroubleDetail(APIView):
