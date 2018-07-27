c2cb10ccb3ccf7e4544aa7cfaa35d131c91c94a7
Toni Robles <r21robles@gmail.com>
2018-07-27 20:09:25 +0200

    Add favicon

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('web.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
 ]
